81500d4153a77191a3c61949516dc26b702ec0cd
Aleksey R <eretik.box@gmail.com>
2022-02-24 00:10:39 +0300

    fix: add "To: " mail header

--- pleroma_sendmail_mention.sh
+++ pleroma_sendmail_mention.sh
@@ -1,3 +1,3 @@
 notifications=$(python3 mstdnotify.py --server_url $1 --token $2 --exclude follow --exclude favourite --exclude reblog --exclude poll --exclude pleroma:emoji_reaction)
 
-[ -z "$notifications" ] || echo "Subject: Pleroma notifications\nContent-Type: text/plain; charset="utf-8"\n\n$notifications" | sendmail -t $3
+[ -z "$notifications" ] || echo "To: $3\nSubject: Pleroma notifications\nContent-Type: text/plain; charset="utf-8"\n\n$notifications" | sendmail -t
