f9b446efe19ad16d1b701801b92dcdfb14fb1671
Aleksey R <eretik.box@gmail.com>
2022-02-23 22:04:30 +0300

    fix: chmod +x

--- pleroma_sendmail_mention.sh
+++ pleroma_sendmail_mention.sh
@@ -1,2 +1,3 @@
 notifications=$(python3 mstdnotify.py --server_url $1 --token $2 --exclude follow --exclude favourite --exclude reblog --exclude poll --exclude pleroma:emoji_reaction)
+
 [ -z "$notifications" ] || echo "Subject: Pleroma notifications\nContent-Type: text/plain; charset="utf-8"\n\n$notifications" | sendmail -t $3
